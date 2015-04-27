28f7c5837834ddf7e7b96314696ad802bf8f0551
Scott Roberts <sroberts@github.com>
2015-04-26 21:26:56 -0400

    just have to finish main() and test

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ You'll need to create a ```.env``` file for API Keys.
 PIPL=aaabbbcccdddeeefffggghhh
 ```
 
-Just don't commit it!
+Just don't commit it! The best way to do that is ```cp .env-sample .env``` and fill in the obviously dummy values.
 
 ### Development
 
--- transforms/freegeoip-geolookup.py
+++ transforms/freegeoip-geolookup.py
@@ -10,57 +10,89 @@ A basic Maltego script to geolocate IPv4Addresses using Freegeoip.net.
 
 Requires the MaltegoTransform Python library in your Python Path.
 https://github.com/sroberts/MaltegoTransform-Python
+
+TODO Has no error handling in the event of a failed lookup or rate limiting.
 """
 
 import requests
 import sys
 
 # from MaltegoTransform import MaltegoEntity
-from MaltegoTransform import MaltegoTransform
+from .MaltegoTransform import MaltegoTransform
 
-def main():
+__version__ == '0.0.1'
 
-    # Set up your Transform object. This is the basis for returning results.
-    trx = MaltegoTransform()
+class GeoLookup(object):
 
-    # The first item on the commandline is the calling object.
-    ipv4address = sys.argv[1]
+    """Adds enhanced geolookups for Maltego"""
 
-    # Now we do some Requests Magic :tm: to geolocate the IP.
-    r = requests.get('https://freegeoip.net/json/{}'.format(ipv4address))
+    def __init__(self, maltegofy=False):
+        self.maltegofy = maltegofy
 
-    # Double check we got back a valid result.
-    if r.status_code == 200:
+    def freegeoip(self, ipv4address):
+        """Looks up ipv4address based on freegeoip.net"""
+
+        # Now we do some Requests Magic :tm: to geolocate the IP.
+        r = requests.get('https://freegeoip.net/json/{}'.format(ipv4address))
+
+        # Set up your Transform object. This is the basis for returning results.
 
         # Parse out the data as JSON.
         data = r.json()
         strings = [data['city'], data['region_name'], data['country_name']]
 
-        # This is a 1-1 transform, so we create one location entity based
-        # on the geolocation data.
-        trx.addEntity("maltego.Location", ', '.join(filter(None, strings)))
+        self._output(strings)
 
-        # This returns the transform as an XML representation that Maltego
-        # uses to update the graph.
-        trx.returnOutput()
+
+    def _output(self, values):
+
+        if self.maltegofy:
+            trx = MaltegoTransform()
+
+            for value in values:
+                trx.addEntity("maltego.Location", value)
+
+            trx.returnOutput()
+        else:
+            sys.stdout.write('\", \"'.join(filter(None, values)))
+
+def main():
+    """Because commandline is always an option."""
+
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    parser.add_argument('-i', '--ioc', dest='ioc', default=None,
+                        help='[OPTIONAL] An IoC to attribute.')
+    parser.add_argument('-m', '--md5', dest='md5', default=None,
+                        help='[OPTIONAL] An MD5 hash.')
+    parser.add_argument('--maltego', dest='maltego', default=False, action='store_true',
+                        help='[OPTIONAL] Run in Maltego compatibility mode.')
+    args, _ = parser.parse_known_args()
+
+    tb = ThreatButt(args.maltego)
+
+    if args.ioc:
+        tb.clown_strike_ioc(args.ioc)
+
+    elif args.md5:
+        tb.bespoke_md5(args.md5)
 
     else:
-        # In the event we can't reach the API end point we want to return an error.
-        if r.status_code == 404:
-            trx.addException("404: Couldn't reach FreeGeoIP.net.")
-        elif r.status_code == 403:
-            trx.addException("404: Lookup limit exceeded.")
-        else:
-            trx.addException("Unknown Error.")
-
-        # ThrowExceptions returns the errored transform.
-        trx.throwExceptions()
+        parser.print_help()
 
 
-if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print "User aborted."
-    except SystemExit:
-        pass
+if __name__ == '__main__':
+    main()
+
+
+
+# # In the event we can't reach the API end point we want to return an error.
+# if r.status_code == 404:
+#     trx.addException("404: Couldn't reach FreeGeoIP.net.")
+# elif r.status_code == 403:
+#     trx.addException("404: Lookup limit exceeded.")
+# else:
+#     trx.addException("Unknown Error.")
+#
+# # ThrowExceptions returns the errored transform.
+# trx.throwExceptions()
